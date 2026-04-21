ef9a1ee0c703f9d5839b7dd0a037b57de993a3c0
unknown <florian.ledu90@gmail.co>
2026-04-21 19:36:39 +0200

    Config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/confcheck/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
