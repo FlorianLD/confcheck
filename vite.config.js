352d1caa24a08aad09ce13b1082760d0de85ae30
unknown <florian.ledu90@gmail.co>
2026-06-11 20:38:00 +0200

    Updated vite config to set correct base for gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Repo name as base so asset URLs work under https://<user>.github.io/confcheck/
+  base: '/confcheck/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
